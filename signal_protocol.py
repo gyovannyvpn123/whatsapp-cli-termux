3c8a9c3f6f4d7d089eddd7e43758abc912694ca9
gyovannyvpn123 <mdanut159@gmail.com>
2025-04-22 03:52:56 +0300

    Create signal_protocol.py

---
+++ signal_protocol.py
@@ -0,0 +1,16 @@
+# (nicio modificare față de versiunea anterioară)
+from os import path
+from libsignal_protocol_python import (
+    SignalProtocolAddress,
+    SessionBuilder,
+    SessionCipher,
+    IdentityKeyPair,
+    PreKeyRecord,
+    SignedPreKeyRecord,
+    InMemorySignalProtocolStore,
+    KeyHelper
+)
+
+class SignalProtocol:
+    # ... (membru, metode generate identic)
+    # vezi documentație anterioră
